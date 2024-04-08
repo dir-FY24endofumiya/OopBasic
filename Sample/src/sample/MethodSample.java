40ac37b33379bb77ddb67998d9570be5fd5c7b45
dir-FY24endofumiya <fumiya.endo.l024@dirbato.co.jp>
2024-04-08 11:03:54 +0900

    L3C2

--- Sample/src/sample/MethodSample.java
+++ Sample/src/sample/MethodSample.java
@@ -11,13 +11,15 @@ public class MethodSample {
         var result2 = sumMethod2(number1 , number2);
         System.out.println("sumMethod2の結果は" + result2);
      }    
+    
     public static void sumMethod1(int num1 , int num2) {
         var result = num1 + num2;
         System.out.println("sumMethod1の結果は" + result);
     }
+    
     public static int sumMethod2(int num1 , int num2) {
-        var result = num1 + num2;
-        return result;
+        var result2 = num1 + num2;
+        return result2;
     }
     
 }
